1fd96c6f2b63a9158b92f682413032475d0776dc
Alexander Rysenko <rysenko@gmail.com>
2013-09-22 14:28:35 +0300

    Session added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
-require('mongoose').connect('mongodb://localhost/gym')
+require('mongoose').connect('mongodb://localhost/gym');
 
 app.use(express.bodyParser());
 app.use(express.static('public'));
+app.use(express.cookieParser());
+app.use(express.session({secret: 'gym-secret'}));
 
 app.use('/clients', require('./routes/clients').middleware);
 
