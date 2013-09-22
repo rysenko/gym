6c359dcdda5134cc121e9e544349979aa5d9d339
Alexander Rysenko <rysenko@gmail.com>
2013-09-22 17:20:30 +0300

    Added resource

--- public/scripts/controllers/clients.js
+++ public/scripts/controllers/clients.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('gym').controller('ClientsCtrl', function ($scope, $resource) {
-    var clientsColl =  $resource('/clients');
-    var clientsRes = $resource('/clients/:id', {id:'@id'}, {update: {method: 'PUT'}});
+    var clientsRead = $resource('/clients');
+    var clientsUpdate = $resource('/clients/:id', {id:'@id'}, {update: {method: 'PUT'}});
 
-    $scope.client = clientsColl.query();
+    $scope.client = clientsRead.query();
     $scope.save = function (client) {
-        clientsRes.update({id:client.id}, client);
+        clientsUpdate.update({id: client.id}, client);
     };
     $scope.add = function() {
-        var newResource = new clientsRes($scope.newClient);
+        var newResource = new clientsUpdate($scope.newClient);
         newResource.$save(function () {
             $scope.clients.push(newResource);
         });
